f1270ae6caa942e990bdaaeac090f854546bcf0d
ahmedasad236 <ahmedasad236@gmail.com>
2024-10-22 12:00:22 +0300

    Modify sentry config files

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -2,11 +2,14 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: "https://7f93d8b12ce8eee4aeeba7cc3dae0874@o4508165201002496.ingest.de.sentry.io/4508165209391184",
+  dsn: 'https://7f93d8b12ce8eee4aeeba7cc3dae0874@o4508165201002496.ingest.de.sentry.io/4508165209391184',
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1
 });
