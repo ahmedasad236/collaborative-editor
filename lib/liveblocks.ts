8747ae5cff138526cb5c6d0506cac3902a692e5a
ahmedasad236 <ahmedasad236@gmail.com>
2024-10-22 14:11:11 +0300

    test the secret key of liveblocks

--- lib/liveblocks.ts
+++ lib/liveblocks.ts
@@ -1,5 +1,7 @@
 import { Liveblocks } from '@liveblocks/node';
 
 export const liveblocks = new Liveblocks({
-  secret: process.env.LIVEBLOCKS_DEV_SECRET_KEY as string
+  // secret: process.env.LIVEBLOCKS_DEV_SECRET_KEY as string
+  secret:
+    'sk_dev_6zybyoYrhNfjhDpGWKGh-ySqvRiWPgbwl08ktd3kNawPy2gTi-cNoGD3ELaJXSbT'
 });
